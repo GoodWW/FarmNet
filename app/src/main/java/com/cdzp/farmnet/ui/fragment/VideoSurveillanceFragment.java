9237deb1e5a829d8c21398bc3a2acbf0556a5e26
张人文 <479696877@qq.com>
2020-01-03 10:26:34 +0800

    视频控制页面添加

--- app/src/main/java/com/cdzp/farmnet/ui/fragment/VideoSurveillanceFragment.java
+++ app/src/main/java/com/cdzp/farmnet/ui/fragment/VideoSurveillanceFragment.java
@@ -22,7 +22,7 @@ public class VideoSurveillanceFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mView = inflater.inflate(R.layout.fragment_timing, container, false);
+        mView = inflater.inflate(R.layout.fragment_video_surveillance, container, false);
         return mView;
     }
 }
