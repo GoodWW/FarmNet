20c87856b03a02854410e6af55a458a848cd673d
张人文 <479696877@qq.com>
2020-01-09 17:59:59 +0800

    登录逻辑提交

--- app/src/main/java/com/cdzp/farmnet/ui/activity/LoginActivity.java
+++ app/src/main/java/com/cdzp/farmnet/ui/activity/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.cdzp.farmnet.ui.activity;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cdzp.farmnet.R;
@@ -8,8 +12,12 @@ import com.cdzp.farmnet.base.BaseView;
 import com.cdzp.farmnet.bean.UserInfo;
 import com.cdzp.farmnet.contract.login.LoginContract;
 import com.cdzp.farmnet.contract.login.LoginPresenter;
+import com.cdzp.farmnet.utils.CountDownTimerUtils;
+import com.cdzp.farmnet.utils.RegexStringUtil;
 import com.cmonbaby.ioc.core.annotation.ContentView;
+import com.cmonbaby.ioc.core.annotation.InjectView;
 import com.cmonbaby.ioc.core.annotation.OnClick;
+import com.qmuiteam.qmui.widget.dialog.QMUITipDialog;
 
 /**
  * 作者：张人文
@@ -19,6 +27,11 @@ import com.cmonbaby.ioc.core.annotation.OnClick;
  */
 @ContentView(R.layout.activity_login)
 public class LoginActivity extends BaseView<LoginPresenter, LoginContract.View> {
+    @InjectView(R.id.etPhone)
+    EditText etPhone;
+    @InjectView(R.id.tvTime)
+    private TextView tvTime;
+    QMUITipDialog tipDialog;
 
     @Override
     public LoginContract.View getContract() {
@@ -28,6 +41,41 @@ public class LoginActivity extends BaseView<LoginPresenter, LoginContract.View> 
 
             }
         };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tipDialog = new QMUITipDialog.Builder(LoginActivity.this)
+                .setIconType(QMUITipDialog.Builder.ICON_TYPE_LOADING)
+                .setTipWord("正在加载")
+                .create(false);
+
+        etPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() > 10) {
+                    if (RegexStringUtil.MatchTelNum(s.toString())) {
+//                        tipDialog.show();
+
+                        CountDownTimerUtils mCountDownTimerUtils = new CountDownTimerUtils(LoginActivity.this ,tvTime, 60000, 1000); //倒计时1分钟
+                        mCountDownTimerUtils.start();
+                    } else {
+                        etPhone.setError("手机号不正确");
+                    }
+                }
+            }
+        });
     }
 
     @Override
